81f3f3f8a5631ee754d592c23aff7c93354b881e
xiao <74530@qq.cn>
2022-06-27 00:50:11 +0800

    第一次提交（测试）

---
+++ src/test/java/com/qinghu/test/HelloTest.java
@@ -0,0 +1,12 @@
+package com.qinghu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloTest {
+    @Test
+    public void test1(){
+
+    }
+}
